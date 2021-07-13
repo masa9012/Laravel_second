dac21a5cbf58542113f23ecc99065247962638b1
EC2 Default User <ec2-user@ip-172-31-24-254.ap-northeast-1.compute.internal>
2021-07-13 13:18:04 +0000

    Laravel-fourth

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'UsersController@index');//書き換え
 
 //ユーザ登録
 Route::get('signup' , 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup' , 'Auth\RegisterController@register')->name('signup.post');
+
+//ログインフォームを表示する
+//ログインフォームに入力された内容(メールアドレス、パスワードなど)を送信する
+//ログアウトを行う
+Route::get('login' , 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login' , 'Auth\LoginController@login')->name('login.post');
+Route::get('logout' , 'Auth\LoginController@logout')->name('logout');
+
+//追記分
+Route::resource('users' , 'UsersController' , ['only' => ['show']]);
+
+Route::group(['middleware' => 'auth'] , function() {
+    Route::resource('movies' , 'MoviesController' , ['only' => ['create' , 'store' , 'destroy']]);
+});
