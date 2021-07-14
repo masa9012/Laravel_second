010eef25f22c5313ddaf89fc34f6ea206db55537
EC2 Default User <ec2-user@ip-172-31-24-254.ap-northeast-1.compute.internal>
2021-07-14 15:07:40 +0000

    Laravel-fifth

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,21 @@ Route::get('login' , 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login' , 'Auth\LoginController@login')->name('login.post');
 Route::get('logout' , 'Auth\LoginController@logout')->name('logout');
 
-//追記分
+
 Route::resource('users' , 'UsersController' , ['only' => ['show']]);
 
-Route::group(['middleware' => 'auth'] , function() {
+Route::group(['prefix' => 'users/{id}'] , function () {
+    Route::get('followings' , 'UsersController@followings')->name('followings');
+    Route::get('followers' , 'UsersController@followers')->name('followers');
+    });
+
+Route::group(['middleware' => 'auth'] , function () {
+    Route::put('users' , 'UsersController@rename')->name('rename');
+    
+    Route::group(['prefix' => 'users/{id}'] , function () {
+        Route::post('follow' , 'UserFollowController@store')->name('follow');
+        Route::delete('unfollow' , 'UserFollowController@destroy')->name('unfollow');
+    });
+    
     Route::resource('movies' , 'MoviesController' , ['only' => ['create' , 'store' , 'destroy']]);
 });
