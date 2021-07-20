6921367957cc83a7dad3587045c2622197447bc7
masahiko.inubushi <inumasa0928@icloud.com>
2021-07-20 05:34:06 +0000

    seventh

--- resources/views/follow/follow_button.blade.php
+++ resources/views/follow/follow_button.blade.php
@@ -4,13 +4,13 @@
 
         @if (Auth::user()->is_following($user->id))
         
-            {!! Form::open(['route' => ['unfollow', $user->id] , 'method' => 'delete']) !!}
+            {!! Form::open(['route' => ['unfollow' , $user->id] , 'method' => 'delete']) !!}
                 {!! Form::submit('このユーザのフォローを外す' , ['class' => "button btn btn-danger mt-1"]) !!}
             {!! Form::close() !!}
             
         @else
         
-            {!! Form::open(['route' => ['follow', $user->id]]) !!}
+            {!! Form::open(['route' => ['follow' , $user->id]]) !!}
                 {!! Form::submit('このユーザをフォローする' , ['class' => "button btn btn-primary mt-1"]) !!}
             {!! Form::close() !!}
             
